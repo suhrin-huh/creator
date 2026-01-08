51f7ca0a7457840c2e87e8b83ed6d6b0045a608a
황민채 <trick0846@naver.com>
2026-01-08 23:54:55 +0900

    feat: Header, Footer 생성

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="p-md h-16 bg-white text-center">
+      <p className="text-caption text-gray-600">© 2026 Creator. All rights reserved.</p>
+    </footer>
+  );
+}
---
+++ src/components/common/Header.tsx
@@ -0,0 +1,23 @@
+export default function Header() {
+  // const navOptions = [
+  //   { label: "가", href: null },
+  //   { label: "나", href: null },
+  //   { label: "다", href: null },
+  //   { label: "라", href: null },
+  // ];
+
+  return (
+    <header className="sticky top-0 z-10 bg-white shadow-sm">
+      <nav className="p-lg flex flex-col items-center justify-between md:flex-row">
+        <div className="logo text-h3 text-primary font-bold">Creator</div>
+        {/* <ul className="nav-links gap-x-md flex">
+            {navOptions.map((option) => (
+              <li className="hover:text-primary-hover" key={`li-${option.label}`}>
+                <a href="#test">{option.label}</a>
+              </li>
+            ))}
+          </ul> */}
+      </nav>
+    </header>
+  );
+}
