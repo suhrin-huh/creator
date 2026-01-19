3ee3c8f5b5d108ee7f5e77eff3d6553729c6f935
황민채 <trick0846@naver.com>
2026-01-20 01:25:55 +0900

    feat: admin 메인 페이지 생성

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+interface AdminRootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function AdminRootLayout ({children}: AdminRootLayoutProps) {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
---
+++ src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function AdminPage() {
+  return (
+    <div>
+      <p>Admin 페이지입니다.</p>
+    </div>
+  );
+}
