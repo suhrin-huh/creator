b72ba85f27c1eb6765ef88cfe70e9c45c12bbe28
황민채 <trick0846@naver.com>
2026-02-15 21:49:10 +0900

    feat: middleware 생성

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+// src/middleware.ts
+
+import { type NextRequest } from 'next/server'
+import { updateSession } from '@/utils/supabase/middleware'
+
+export async function middleware(request: NextRequest) {
+  return await updateSession(request)
+}
+
+export const config = {
+  matcher: [
+    /*
+     * 정적 파일(_next/static, _next/image, favicon 등)은 제외
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+}
