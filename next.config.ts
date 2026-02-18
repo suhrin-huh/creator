0e8a70244cbe28a4bca73f475fa41e0f91105f28
황민채 <trick0846@naver.com>
2026-02-18 20:36:37 +0900

    fix: next.config images remotePatterns수정

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL("https://images.unsplash.com/**")],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "yyqkemabxechuzdfnwno.supabase.co",
+        pathname: "/storage/v1/object/public/**", // 특정 경로만 허용 (보안상 권장)
+      },
+    ],
   },
   /*body 제한 1MB(default) -> 5MB로 변경 */
   experimental: {
     serverActions: {
-      bodySizeLimit: '5mb', // 제한을 5MB로 상향 (기본값: 1MB)
+      bodySizeLimit: "5mb", // 제한을 5MB로 상향 (기본값: 1MB)
     },
   },
 };
