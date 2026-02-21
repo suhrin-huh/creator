bbfb934f2a0e7f14a784a1a2001dccfb06bed598
황민채 <trick0846@naver.com>
2026-02-21 17:49:36 +0900

    fix: ProductCard 제품 정보 스타일 수정

--- src/app/(public)/(main)/_components/ProductCard.tsx
+++ src/app/(public)/(main)/_components/ProductCard.tsx
@@ -15,7 +15,7 @@ export default function ProductCard({ image, title, description, link }: Product
   return (
     <Link
       href={link}
-      className="px-lg flex w-full flex-row items-center overflow-hidden rounded-xl bg-white transition duration-100 hover:shadow-lg active:scale-105"
+      className="p-md flex w-full flex-row items-center overflow-hidden rounded-xl bg-white transition duration-100 hover:shadow-lg active:scale-105"
     >
       {/* 이미지 컨테이너 */}
       <div className="relative h-20 w-20 min-w-20 shrink-0">
@@ -28,10 +28,10 @@ export default function ProductCard({ image, title, description, link }: Product
         />
       </div>
 
-      {/* 제품 정보 TODO: 제품 정보에 따른 크기 변동으로 인해 스타일 오류(line-clamp-2)*/}
-      <div className="flex-1 p-4">
-        <h3 className="mb-2 text-lg font-bold text-gray-800">{title}</h3>
-        <p className="line-clamp-2 text-sm leading-relaxed text-gray-600">{description}</p>
+      {/* 제품 정보 TODO: 제품 정보에 따른 크기 변동으로 인해 스타일 오류 수정*/}
+      <div className="p-md flex-md min-w-0">
+        <h3 className="text-md mb-2 font-bold text-gray-800">{title}</h3>
+        <p className="truncate text-xs text-gray-600">{description}</p>
       </div>
     </Link>
   );
