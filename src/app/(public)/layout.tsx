5ff2b2d2367a632b8af6576fd63133589e58c3fe
황민채 <trick0846@naver.com>
2026-03-05 20:56:01 +0900

    feat: Header 컴포넌트 props 추가

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -9,7 +9,7 @@ interface PublicRootLayoutProps {
 export default function PublicRootLayout({ children }: PublicRootLayoutProps) {
   return (
     <div className="hide-scrollbar relative flex max-w-175 min-w-75 flex-1 flex-col overflow-scroll md:rounded-lg md:shadow-xl">
-      <Header />
+      <Header title={"Creator"} />
       {children}
       <Footer />
     </div>
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,8 +1,12 @@
-export default function Header() {
+interface HeaderProps {
+  title: string;
+}
+
+export default function Header({ title }: HeaderProps) {
   return (
     <header className="sticky top-0 z-10 bg-white shadow-md md:rounded-t-lg">
       <nav className="p-md flex flex-col items-center justify-between md:flex-row">
-        <div className="text-h3 text-primary font-semibold">Creator</div>
+        <div className="text-h3 text-primary font-semibold">{title}</div>
       </nav>
     </header>
   );
