cdf5d1285e33c52005e6b70483c5fb40157fa5a3
황민채 <trick0846@naver.com>
2026-04-16 19:25:12 +0900

    feat: PixelBackground 생성

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,6 +1,7 @@
 // components
 import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
+import PixelBackground from "@/components/common/PixelBackground";
 
 interface PublicRootLayoutProps {
   children: React.ReactNode;
@@ -9,6 +10,7 @@ interface PublicRootLayoutProps {
 export default function PublicRootLayout({ children }: PublicRootLayoutProps) {
   return (
     <div className="bg-main-gradient hide-scrollbar gap-sm p-md relative flex max-w-175 min-w-78 flex-1 flex-col overflow-scroll md:rounded-lg md:shadow-xl">
+      <PixelBackground />
       <Header title={"Creator"} />
       {children}
       <Footer />
