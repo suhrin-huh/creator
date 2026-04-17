39782555927f73357a2e2a22a804cc2ab3dfdc86
황민채 <trick0846@naver.com>
2026-04-17 11:15:01 +0900

    hotfix: 중복되는 최소 크기 설정 제거

--- src/app/(public)/(main)/_components/MainContainer.tsx
+++ src/app/(public)/(main)/_components/MainContainer.tsx
@@ -5,7 +5,7 @@ import ProductSection from "./ProductSection";
 export default function MainContainer() {
   return (
     // 메인 컨텐츠
-    <main className="gap-md font-ko text-text-main flex min-h-screen min-w-87.5 flex-1 flex-col overflow-x-hidden">
+    <main className="gap-md font-ko text-text-main flex flex-1 flex-col overflow-x-hidden">
       {/* 프로필 */}
       <ProfileSection />
       {/* 광고제품 리스트 */}
